c984eb121571b5ddb92f19cc47094420a39faf5c
Alex Shumilov <ashumilov@pivotal.io>
2018-05-02 09:35:08 -0500

    first commit

---
+++ build-pipeline.groovy
@@ -0,0 +1,61 @@
+node(label: 'fortify'){
+    String projectAcronym = params['PROJECT_ACRONYM'] ?: error('Mandatory parameter PROJECT_ACRONYM was not provided.')
+    String gitCloneUrl = params['GIT_CLONE_URL'] ?: error('Mandatory parameter GIT_CLONE_URL was not provided.')
+
+    String appPackagingType = params['APP_PACKAGING_TYPE'] ?: 'jar'
+    String artifactSuffix = appPackagingType.toLowerCase()
+
+    if (artifactSuffix != 'war' && artifactSuffix != 'jar') error('APP_PACKAGING_TYPE provided should be either war or jar.')
+
+    String artifactoryServerId = params['ARTIFACTORY_SERVER_ID'] ?: 'artifactory_qa'
+    String artifactoryBuildRepo = params['ARTIFACTORY_BUILD_REPO'] ?: 'lib-stapshot-local'
+    String mvnToolId = params['MVN_TOOL_ID'] ?: 'Maven 3.0.5'
+
+    stage('Preparation'){
+        git gitCloneUrl
+        mvnHome = tool mvnToolId
+    }
+
+    stage('Build'){
+        if (isUnix()) {
+            sh "'${mvnHome}/bin/mvn' clean package"
+        }
+    }
+
+    stage('Results'){
+        junit '**/target/surefire-reports/TEST-*.xml'
+
+        sh 'rm -rf release-archive tarball && mkdir -p release-archive tarball'
+        sh "cp target/*.${artifactSuffix} release-archive/"
+        sh "cd release-archive && tar cfzf ../tarball/${projectAcronym}-build-${BUILD_NUMBER}.tgz . && cd -"
+
+        archive 'tarball/*.tgz'
+    }
+
+    stage('Upload build to Artifactory'){
+        def server = Artifactory.server artifactoryServerId
+        def uploadSpec = """{
+        "files":[
+            {
+                "pattern": "tarball/*.tgz",
+                "target": "${artifactoryBuildRepo}/${projectAcronym}/new-builds"
+            }
+        ]
+        }"""
+        server.upload(uploadSpec)
+    }
+
+    stage('Prepare next dev staging build in Artifactory'){
+        sh "rm -rf dev-staging-tarball && mkdir -p dev-staging-tarball && cp tarball/*.tgz dev-staging-tarball/${projectAcronym}-dev-staging.tgz"
+        def server = Artifactory.server artifactoryServerId
+        def uploadSpec = """{
+        "files":[
+            {
+                "pattern": "dev-staging-tarball/*.tgz",
+                "target": "${artifactoryBuildRepo}/${projectAcronym}/cloud-dev-staging/"
+            }
+        ]
+        }"""
+        server.upload(uploadSpec)
+    }
+}
---
+++ deploy-dev-pipeline.groovy
@@ -0,0 +1,46 @@
+node(label: 'fortify') {
+    String projectAcronym = params['PROJECT_ACRONYM'] ?: error('Mandatory parameter PROJECT_ACRONYM was not provided.')
+    String appPackagingType = params['APP_PACKAGING_TYPE'] ?: 'jar'
+    String artifactSuffix = appPackagingType.toLowerCase()
+
+    if (artifactSuffix != 'war' && artifactSuffix != 'jar') error('APP_PACKAGING_TYPE provided should be either war or jar.')
+
+    String artifactoryServerId = params['ARTIFACTORY_SERVER_ID'] ?: 'artifactory_qa'
+    String artifactoryBuildRepo = params['ARTIFACTORY_BUILD_REPO'] ?: 'lib-stapshot-local'
+
+    String pcfDevCredentialsId = params['PCF_DEV_CREDENTIALS_ID'] ?: 'pcf-dev-space'
+//todo fix below
+    String pcfDevApiTarget = params['PCF_DEV_API_TARGET'] ?: 'api.run.pivotal.io'
+    String pcfDevOrg = params['PCF_DEV_ORG'] ?: 'org-name'
+    String pcfDevSpace = params['PCF_DEV_SPACE'] ?: 'space-name'
+
+    stage('Get artifact from repository'){
+        def server = Artifactory.server artifactoryServerId
+        def downloadSpec = """{
+        "files":[
+            {
+                "pattern": "${artifactoryBuildRepo}/${projectAcronym}/cloud-dev-staging/*.tgz",
+                "target": "release-tarball/",
+                "flat": true
+            }
+        ]
+        }"""
+        server.download(downloadSpec)
+
+        sh 'tar xvzf release-tarball/*.tgz'
+        sh 'rm -rf target && mkdir -p target'
+        mv "mv *.${appPackagingType} target/"
+    }
+
+    stage('Push to development environment on PCF'){
+        timeout(time: 300, unit: 'SECONDS'){
+            pushToCloudFoundry(
+                    target: pcfDevApiTarget,
+                    organization: pcfDevOrg,
+                    credentialsId: pcfDevCredentialsId,
+                    selfSigned: true,
+                    pluginTimeout: 300
+            )
+        }
+    }
+}
